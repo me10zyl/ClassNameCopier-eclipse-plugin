dd28c0f35c21f74f2ffd57ccb56a87a83f1efb8d
zyl <zengyil@sunyuki.com>
2015-07-09 15:02:22 +0800

    update website

---
+++ ClassNameCopier.website/upload.bat
@@ -0,0 +1,3 @@
+cd e:\JavaEE\ClassNameCopier\ClassNameCopier.website\
+e:
+rsync --delete -rPv --exclude 'upload.bat' * root@zyl-me.xicp.net:/mnt/diskd7/linux/tomcat/apache-tomcat-7.0.54/webapps/classnamecopier/
